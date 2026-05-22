8cf6f6acb3b6eda81a5dcbb03a4d401ba9a12076
hwihyun <hwihyun.kim@skylabs.io>
2026-05-22 09:27:05 +0900

    change sqlite to postgresql

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "sqlite:///./logistics.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
