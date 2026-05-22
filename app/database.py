adb339bdd3547c2814f3c3a5f201ae4e4bcf3ee9
hwihyun <hwihyun.kim@skylabs.io>
2026-05-22 09:51:50 +0900

    fix postgres driver

--- app/database.py
+++ app/database.py
@@ -6,10 +6,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(
     DATABASE_URL,
-    connect_args={
-        "check_same_thread": False,
-        "timeout": 30
-    }
+    pool_pre_ping=True
 )
 
 SessionLocal = sessionmaker(bind=engine)
